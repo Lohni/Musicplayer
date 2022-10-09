0643807f0517f05c58fc8772381dd5f1cd934f07
Andreas Lohninger <andi.lohninger@hotmail.com>
2022-10-09 20:12:28 +0200

    create settings fragment #26

---
+++ app/src/main/java/com/example/musicplayer/ui/settings/SettingFragment.java
@@ -0,0 +1,32 @@
+package com.example.musicplayer.ui.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.musicplayer.R;
+
+public class SettingFragment extends Fragment {
+    private SharedPreferences sharedPreferences;
+
+    public SettingFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        return view;
+    }
+}
